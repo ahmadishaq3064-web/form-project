e75dfb1be19a67cc0eaecca9abf4753da763d00b
Ahmad Ishaq <ahmadishaq3064@gmail.com>
2026-08-24 10:46:00 +0500

    add a logo on tab

--- resources/views/user-data.blade.php
+++ resources/views/user-data.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <!-- Your CSS -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="icon" type="image/png" href="images.jpg">
 </head>
 <body>
     <!-- Success popup -->
